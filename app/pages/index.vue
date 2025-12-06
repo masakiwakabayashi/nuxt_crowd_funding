4cdd3c4c7660144a0a98d73f76156029e3e4fd13
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-12-06 14:14:14 +0900

    ページコンポーネントのディレクトリ構成を変更

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <!-- 開発するページをトップページで読み込んで、実際のページコンポーネントで開発を進める -->
 <script setup lang="ts">
-  import CrowdfundingPage from '~/pages/organizations/[organizationId]/projects/[projectId]/crowdfunding/index.vue'
+  import CrowdfundingPage from '~/pages/organizations/[organizationId]/crowdfunding/index.vue'
 </script>
 
 <template>
