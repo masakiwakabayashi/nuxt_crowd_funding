c19968d9ba39a6cc776126ff9c77ed78ffab2ef5
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-12-16 20:59:00 +0900

    全画面のデザインになるように修正

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@
     <div
       class="flex min-h-screen flex-col px-4 pb-10 pt-6 text-slate-900 sm:px-8 lg:px-12"
     >
-      <div class="mx-auto flex w-full max-w-6xl flex-1 flex-col gap-6">
+      <div class="flex w-full flex-1 flex-col gap-6">
         <Header />
         <slot />
       </div>
