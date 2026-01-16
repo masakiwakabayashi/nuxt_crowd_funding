b62f21a7dc5b16f4dff51108baca1710f4dc9f20
masakiwakabayashi <masaki.wakabaya@gmail.com>
2026-01-16 10:39:16 +0900

    スキーマの型定義を修正

--- server/schemas/deliveries.ts
+++ server/schemas/deliveries.ts
@@ -1,17 +1,17 @@
 import z from 'zod'
 import { optionalRelation } from './utils'
 import { supporterSchema } from './supporters'
+import type { DeliveryStatus } from '@/shared/types/Delivery'
+
+
+export const deliveryStatusSchema = z.custom<DeliveryStatus>()
 
 export const deliverySchema = z.object({
   id: z.string(),
   project_id: z.string(),
   reward_id: z.string(),
   supporter_id: z.string(),
-  status: z.union([
-    z.literal('未着手'),
-    z.literal('作成中'),
-    z.literal('完了'),
-  ]),
+  status: deliveryStatusSchema,
   created_at: z.string(),
   updated_at: z.string(),
   due_date: z.string(),
